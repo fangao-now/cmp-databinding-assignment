2eac49eef5c8755fe1306de8fe0d4f00fdd7e38f
coolcrazydog <fan.gao1989@gmail.com>
2019-03-01 22:47:13 -0500

    Game start and stop now could send event to app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  counter = 0;
+
+  onGameStarted() {
+    this.counter += 1;
+    console.log(this.counter);
+  }
+
+  onGameStopped() {
+    this.counter = 0;
+    console.log('Counter reset in app-component!');
+  }
 }
--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() gameStarted = new EventEmitter<{}>();
+  @Output() gameStopped = new EventEmitter<{}>();
+  isGameStarted = false;
+  counterStart: any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartGame() {
+    if (!this.isGameStarted) {
+      this.isGameStarted = true;
+      this.counterStart = setInterval(() => { this.gameStarted.emit(); }, 1000);
+      console.log('Game started in game-control-component!');
+    }
+  }
+
+  onStopGame() {
+    if (this.isGameStarted) {
+      this.isGameStarted = false;
+      clearInterval(this.counterStart);
+      this.gameStopped.emit();
+      console.log('Game stopped in game-control-component!');
+    }
   }
 
 }
