456193094e2cbb1ec48d58f8dd81f8fe0c7e7605
coolcrazydog <fan.gao1989@gmail.com>
2019-03-01 23:06:59 -0500

    implemented odd/even component display with Input property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   counter = 0;
+  oddNumberArrary: number[] = [];
+  evenNumberArrary: number[] = [];
 
   onGameStarted() {
     this.counter += 1;
+    if (this.counter % 2 === 1) {
+      this.oddNumberArrary.push(this.counter);
+    } else {
+      this.evenNumberArrary.push(this.counter);
+    }
     console.log(this.counter);
   }
 
